96b471d751602b28e7dfb27ae2f301c45ef7a648
zsnydr <zackwsnyder@gmail.com>
2016-10-17 20:02:01 -0700

    [REFACTOR] quick fix

--- Client/app/queue/queue.js
+++ Client/app/queue/queue.js
@@ -18,7 +18,6 @@ angular.module('app.queue', [])
       })
   }
 
-<<<<<<< e55f98b4b2fb47f1dea7473eaf68b793b5e9e160
 $scope.ticket = {};
   $scope.addTicket = function () {
     console.log('inside addTicket module ', $scope.ticket)
@@ -32,8 +31,6 @@ $scope.ticket = {};
     });
   }
 
-=======
->>>>>>> [FIX] Work on proper auth and redirect handling
   initializeQueue();
 
 }])
